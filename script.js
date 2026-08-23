96e4d039f6e298486228aa642321c2f2387d8de1
Peachcoding97 <187660744+Peachboy97@users.noreply.github.com.>
2026-08-23 18:07:32 +0100

    created functions for the basic maths operators, such as add, subtract, etc

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-console.log("Hello world, let's get to work")
+// console.log("Hello world, let's get to work")
+
+
+function addition (numberA, numberB) {
+    let sum = numberA + numberB;
+    return sum;
+}
+
+
+function subtraction (numberA, numberB) {
+    let sum = numberA - numberB;
+    return sum;
+}
+
+function multiplication (numberA, numberB) {
+    let sum = numberA * numberB;
+    return sum;
+}
+
+function division (numberA, numberB) {
+    let sum = numberA / numberB;
+    return sum
+}
+
+
 
