fda7a0b985881bb1e5b5e5469ccff92c78231176
Peachcoding97 <187660744+Peachboy97@users.noreply.github.com.>
2026-08-24 13:29:34 +0100

    Changed valueA value to an empty array. Then made changes to the event listener for number three to add three to valueA and to display it on the calculator screen. Created an event listener for the number one

--- script.js
+++ script.js
@@ -22,7 +22,46 @@ function division (numberA, numberB) {
     return sum
 }
 
-let valueA = "number goes here";
-let valueB = "Second number goes here";
-let operator = "This is where +, -, *, / goes";
+let valueA = [];
+let valueB = 0;
+let operator = ["*", "+", "-", "/"]
+
+
+function operate (valueA, valueB) {
+
+     if (operator[0] == "*"){
+
+        return multiplication(valueA, valueB)
+
+     }
+
+}
+
+
+
+
+// Below is the code for the buttons 
+
+const displayScreen = document.querySelector(".display")
+
+
+const one = document.querySelector("#one");
+const three = document.querySelector("#three");
+
+one.addEventListener("click", () => {
+    valueA += 1;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+})
+
+three.addEventListener("click", () => {
+    valueA += 3;
+    console.log(valueA);
+    console.log(typeof(valueA));
+    displayScreen.textContent = valueA;
+    
+})
+
+
+
 
