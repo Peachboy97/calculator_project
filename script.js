5d19316d34755f15f6aecdcc748ac50e8099729b
Peachcoding97 <187660744+Peachboy97@users.noreply.github.com.>
2026-08-24 13:44:51 +0100

    Added the query selector for all the number buttons and the eventlistener to add the number into valueA. As of now any number that is put into valueA is a string, so I will need to convert valueA to a number at some point

--- script.js
+++ script.js
@@ -46,13 +46,29 @@ const displayScreen = document.querySelector(".display")
 
 
 const one = document.querySelector("#one");
+const two = document.querySelector("#two");
 const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+
+
 
 one.addEventListener("click", () => {
     valueA += 1;
     console.log(valueA);
     displayScreen.textContent = valueA
-})
+});
+
+two.addEventListener("click", () => {
+    valueA += 2;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
 
 three.addEventListener("click", () => {
     valueA += 3;
@@ -60,7 +76,49 @@ three.addEventListener("click", () => {
     console.log(typeof(valueA));
     displayScreen.textContent = valueA;
     
-})
+});
+
+four.addEventListener("click", () => {
+    valueA += 4;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
+
+five.addEventListener("click", () => {
+    valueA += 5;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
+
+six.addEventListener("click", () => {
+    valueA += 6;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
+
+seven.addEventListener("click", () => {
+    valueA += 7;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
+
+eight.addEventListener("click", () => {
+    valueA += 8;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
+
+nine.addEventListener("click", () => {
+    valueA += 9;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
+
+zero.addEventListener("click", () => {
+    valueA += 0;
+    console.log(valueA);
+    displayScreen.textContent = valueA
+});
 
 
 
